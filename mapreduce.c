0a7b98638da871a1e5e00b02f9333caf45f2b747
diva D <jinbar@outlook.com>
2020-03-31 15:11:30 -0500

    some formatting

--- mapreduce.c
+++ mapreduce.c
@@ -6,46 +6,63 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
-typedef struct Node { 
+struct Node { 
     char *key;
-	int val;
+	char* value;
     struct Node *next; 
-} Node;
+};
 
 char* global_map[5381];
-Node* unique[5381];
-int map_index = 0;
-int combine_index = 0;
+struct Node* unique[5381];
 
-void push(struct Node** head_ref, char* new_key) {
+void push(struct Node** head_ref, char* new_key, char* new_value) {
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-    new_node->key  = new_key; 
-    new_node->next = (*head_ref); 
-    (*head_ref) = new_node; 
+    new_node->key = new_key;
+    new_node->value = new_value;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
     unsigned long hash = 5381;
     int c;
-    while ((c = *key++) != '\0')
+    while ((c = *key++) != '\0') {
         hash = hash * 33 + c;
+    }
     return hash % num_partitions;
 }
 
 void MR_EmitToCombiner(char *key, char *value) {
-	global_map[map_index] = key;
-	map_index++;
+    unsigned long index = MR_DefaultHashPartition(key, 5381);
+    if (unique[index] == NULL) {
+        struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+        new_node->key = key;
+        new_node->value = value;
+    } else {
+        // Iterate to see if we find an existing node with the key, and then increment the value
+        struct Node* runner = unique[index];
+        int found = 0;
+        while (runner != NULL) {
+            if (strcmp(runner->key, key) == 0) {
+                int temp_int = atoi(runner->value);
+                temp_int++;
+                char* temp_str = (char*)malloc(10 * sizeof(char));
+                sprintf(temp_str, "%d", temp_int);
+                runner->value = temp_str;
+                found = 1;
+                break;
+            }
+            runner = runner->next;
+        }
+        if (found == 0) {
+            push(&(unique[index]), key, value);
+        }
+    }
 	printf("%s %s\n", key, value);
 }
 
 void MR_EmitToReducer(char *key, char *value) {
 	printf("%s %s\n", key, value);
-}
-
-char* get_next_cg(char *key) {
-	char * temp = global_map[combine_index];
-	combine_index++;
-	return(temp);
 }
 
 void MR_Run(int argc, char *argv[],
@@ -56,5 +73,4 @@ void MR_Run(int argc, char *argv[],
 			// at this point all we have is an array with all the individual words.
 			// also they're all 1 so theres no point in adding
 			map(argv[1]);
-
 		}
