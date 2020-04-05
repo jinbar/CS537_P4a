735331a2c972760c3933c24b7fa46802fb0e10d3
diva D <jinbar@outlook.com>
2020-04-05 00:37:50 -0500

    finally works sequentially

--- mapreduce.c
+++ mapreduce.c
@@ -8,6 +8,8 @@
 
 // TODO: expand the arrays if it overflows in size
 // TODO: figure out when we free all of the things we've allocated so far
+// TODO: be able to do this on multiple files
+// TODO: try copying it bit by bit. YOUR POINTERS ARE WACK
 
 struct Node { 
     char *key;
@@ -26,32 +28,34 @@ char** global_map;
 int global_index = 0;
 
 char** unique_tracker;
-int unique_tracker_index = 1;
+int unique_tracker_index = 0;
+
+char** all_unique_tracker;
+int all_unique_tracker_index = 0;
 
 struct Node** unique;
 int get_next_combine_index = 0;
-int get_next_reduce_index = 0;
 
-void push_unique(struct Reducer_Node** head, struct Node** combined_map) {
-    struct Reducer_Node* new_node = (struct Reducer_Node*) malloc(sizeof(struct Reducer_Node));
-    new_node->combined_map = combined_map;
-    new_node->next = *head;
-    *head = new_node;
-    reducer_head_runner = *head;
-}
-
-void push_word(unsigned long index, char* new_key, char* new_value) {
+void push_word(struct Node ** head, char* new_key, char* new_value) {
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
     new_node->key = (char*) malloc(sizeof(new_key));
     new_node->value = (char*) malloc(sizeof(new_value));
-    for (int i = 0; i < sizeof(new_key); i++) {
-        *(char *)(new_node->key + i) = *(char *)(new_key + i);
+    strcpy(new_node->key, new_key);
+    strcpy(new_node->value, new_value);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void push_unique(struct Reducer_Node** head, struct Node** combined_map) {
+    struct Reducer_Node* new_node = (struct Reducer_Node*) malloc(sizeof(struct Reducer_Node));
+    new_node->combined_map = (struct Node**) malloc(10000 * sizeof(struct Node *));
+    for (int i = 0; i < unique_tracker_index; i++) {
+        unsigned long index = MR_DefaultHashPartition(unique_tracker[i], 10000);
+        push_word(&new_node->combined_map[index], combined_map[index]->key, combined_map[index]->value);
     }
-    for (int i = 0; i < sizeof(new_value); i++) {
-        *(char *)(new_node->value + i) = *(char *)(new_value + i);
-    }
-    new_node->next = unique[index];
-    unique[index] = new_node;
+    new_node->next = *head;
+    *head = new_node;
+    reducer_head_runner = *head;
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
@@ -66,18 +70,32 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
 void MR_EmitToCombiner(char *key, char *value) {
     strcpy(global_map[global_index], key);
     global_index++;
-    for (int i = 0 ; i < unique_tracker_index; i++) {
+
+    int i;
+    for (i = 0 ; i < unique_tracker_index; i++) {
         if (strcmp(key, unique_tracker[i]) == 0) {
+            break;
+        }
+    }
+
+    if (i == unique_tracker_index) {
+        strcpy(unique_tracker[unique_tracker_index], key);
+        unique_tracker_index++;        
+    }
+
+    for (i = 0 ; i < all_unique_tracker_index; i++) {
+        if (strcmp(key, all_unique_tracker[i]) == 0) {
             return;
         }
     }
-    strcpy(unique_tracker[unique_tracker_index - 1], key);
-    unique_tracker_index++;
+    
+    strcpy(all_unique_tracker[all_unique_tracker_index], key);
+    all_unique_tracker_index++;        
 }
 
 void MR_EmitToReducer(char *key, char *value) {
 	unsigned long index = MR_DefaultHashPartition(key, 10000);
-    push_word(index, key, value);
+    push_word(&unique[index], key, value);
 }
 
 char* get_next_combine(char* key) {
@@ -92,19 +110,17 @@ char* get_next_combine(char* key) {
 
 char* get_next_reduce(char* key, int partition_number) {
     unsigned long index = MR_DefaultHashPartition(key, 10000);
-    while (reducer_head_runner != NULL) {
-        struct Node * runner = reducer_head_runner->combined_map[index];
-        while (runner != NULL) {
-            if (strcmp(runner->key, key) == 0) {
-                reducer_head_runner = reducer_head_runner->next;
-                return runner->value;
-            }
-            runner = runner->next;
-        }
-        reducer_head_runner = reducer_head_runner->next;
+    if (reducer_head_runner == NULL) {
+        reducer_head_runner = reducer_head;
+        return NULL;
     }
-    reducer_head_runner = reducer_head;
-    return NULL;
+    if (reducer_head_runner->combined_map[index] == NULL) {
+        reducer_head_runner = reducer_head_runner->next;
+        return "0";
+    }
+    char * temp = reducer_head_runner->combined_map[index]->value;
+    reducer_head_runner = reducer_head_runner->next;
+    return temp;
 }
 
 void MR_Run(int argc, char *argv[],
@@ -115,26 +131,31 @@ void MR_Run(int argc, char *argv[],
             // Allocate memory for everything
             global_map = (char **) malloc(10000 * sizeof(char *));
             unique_tracker = (char **) malloc(10000 * sizeof(char *));
-            unique = (struct Node**) malloc(10000 * sizeof(struct Node *));
+            all_unique_tracker = (char **) malloc(10000 * sizeof(char *));
             for (int i = 0; i < 10000; i++) {
                 global_map[i] = (char * ) malloc(30 * sizeof(char));
                 unique_tracker[i] = (char *) malloc(30 * sizeof(char));
+                all_unique_tracker[i] = (char *) malloc(30 * sizeof(char));
             }
+            unique = (struct Node**) malloc(10000 * sizeof(struct Node *));
+            for (int a = 1; a < argc; a++) {
+                // Now map is all the of words. might not be unique
+                map(argv[a]);
 
-            // Now map is all the of words. might not be unique
-			map(argv[1]);
+                // Combining all of the unique words. Now unique has all the unique word counts
+                for (int i = 0; i < unique_tracker_index; i++) {
+                    combine(unique_tracker[i], get_next_combine);
+                    get_next_combine_index = 0;
+                }
 
-            // Combining all of the unique words. Now unique has all the unique word counts
-            for (int i = 0; i < unique_tracker_index; i++) {
-                combine(unique_tracker[i], get_next_combine);
-                get_next_combine_index = 0;
+                // Add all of the combined unique maps to our reduced linked list
+                push_unique(&reducer_head, unique);
+
+                global_index = 0;
+                unique_tracker_index = 0;
             }
-
-            // Add all of the combined unique maps to our reduced linked list
-            push_unique(&reducer_head, unique);
-
-            // We reduce
-            for (int i = 0; i < unique_tracker_index; i++) {
-                reduce(unique_tracker[i], NULL, get_next_reduce, 1);
+            for (int i = 0; i < all_unique_tracker_index; i++) {
+                reduce(all_unique_tracker[i], NULL, get_next_reduce, 1);
+                reducer_head_runner = reducer_head;
             }
 		}
