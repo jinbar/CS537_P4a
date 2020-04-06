93f249b5a79352e7fa1b1065b777295eacd3994d
diva D <jinbar@outlook.com>
2020-04-05 20:53:02 -0500

    idk

--- mapreduce.c
+++ mapreduce.c
@@ -6,26 +6,22 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 
-// TODO: expand the arrays if it overflows in size
-// TODO: figure out when we free all of the things we've allocated so far
-
+// Defining variables
 struct Node { 
     char *key;
 	char *value;
     struct Node *next; 
 };
-
 struct Node** reduce_map;
+struct Node** combine_map;
 
-void push_node(struct Node ** head, char* new_key, char* new_value) {
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
-    new_node->key = (char*) malloc(30 * sizeof(char));
-    new_node->value = (char*) malloc(30 * sizeof(char));
-    strcpy(new_node->key, new_key);
-    strcpy(new_node->value, new_value);
-    new_node->next = *head;
-    *head = new_node;
-}
+char** unique_tracker;
+int unique_tracker_index = 0;
+
+// Helper functions
+void MR_EmitToCombiner_Helper(char* key, char* value, struct Node** head);
+void add_node(char* key, char* value, struct Node** head);
+char* delete_node(struct Node **head_ref, char* key);
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
     unsigned long hash = 5381;
@@ -37,25 +33,57 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
 }
 
 void MR_EmitToCombiner(char *key, char *value) {
-    struct Node** combine_map;
-    combine_map = (struct Node**) malloc(1000 * sizeof(struct Node*));
-    MR_EmitToCombiner_Helper(key, value, *combine_map);
+    unsigned long index = MR_DefaultHashPartition(key, 1000);
+    add_node(key, value, &combine_map[index]);
+
+    for (int i = 0; i < unique_tracker_index; i++) {
+        if (strcmp(unique_tracker[i], key) == 0) {
+            unique_tracker_index++;
+            return;
+        }
+    }
+    strcpy(unique_tracker[unique_tracker_index], key);
+    unique_tracker_index++;
 }
 
-void MR_EmitToCombiner_Helper(char* key, char* value, struct Node*** combine_map) {
-    
+void add_node(char* key, char* value, struct Node** head) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->key = (char*) malloc(30 * sizeof(char));
+    new_node->value = (char*) malloc(30 * sizeof(char));
+    strcpy(new_node->key, key);
+    strcpy(new_node->value, value);
+    new_node->next = *head;
+    *head = new_node;
+}
+
+char* delete_node(struct Node **head_ref, char* key) {
+    struct Node* temp = *head_ref;
+    struct Node* prev = temp; 
+    if (temp != NULL && strcmp(temp->key, key) == 0) {
+        *head_ref = temp->next;
+        free(temp);
+        return key; 
+    }
+    while (temp != NULL && strcmp(temp->key, key) != 0) {
+        prev = temp; 
+        temp = temp->next; 
+    } 
+    if (temp == NULL) {
+        return NULL;
+    }
+    prev->next = temp->next;
+    free(temp);
+    return key;
+} 
+
+char* get_next_combine(char* key) {
+    unsigned long index = MR_DefaultHashPartition(key, 1000);
+    // printf("%s\n", key);
+    return delete_node(&combine_map[index], key);
 }
 
 void MR_EmitToReducer(char *key, char *value) {
-
-}
-
-char* get_next_combine(char* key) {
-
-}
-
-char* get_next_reduce(char* key, int partition_number) {
-
+    printf("%s:%s\n", key, value);
 }
 
 void MR_Run(int argc, char *argv[],
@@ -63,6 +91,19 @@ void MR_Run(int argc, char *argv[],
         Reducer reduce, int num_reducers,
         Combiner combine,
         Partitioner partition) {
+
+            // Allocate things
+            unique_tracker = (char**) malloc(1000 * sizeof(char*));
+            for (int i = 0; i < 1000; i++) {
+                unique_tracker[i] = (char*) malloc(30 * sizeof(char));
+            }
+            combine_map = (struct Node**) malloc(1000 * sizeof(struct Node*));
+        
             map(argv[1]);
+            for (int i = 0; i < unique_tracker_index; i++) {
+                combine(unique_tracker[i], get_next_combine);
+                unique_tracker_index = 0;
+            }
+            
 		}
 
